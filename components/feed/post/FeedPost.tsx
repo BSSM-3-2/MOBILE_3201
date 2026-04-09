e08bc6f10521eb3077c095c78201c1294aff06b1
kongduk <gongduk4960@naver.com>
2026-04-09 11:30:38 +0900

    ftr: 이미지 핀치 줌 및 더블탭 하트 구현

--- components/feed/post/FeedPost.tsx
+++ components/feed/post/FeedPost.tsx
@@ -7,16 +7,31 @@ import { FeedPostCaption } from './FeedPostCaption';
 import { ThemedView } from '@components/themed-view';
 import FeedImage from '@components/feed/post/FeedImage';
 import { resolveImageSource } from '@/utils/image';
+import { useFeedStore } from '@/store/feed-store';
 
 function FeedPost({ post }: { post: Post }) {
     const user = post.author;
+    const { posts, toggleLike } = useFeedStore();
 
     if (!user) return null;
+
+    // TODO: 최신 liked 상태 가져오기 (실습 3-6)
+    const currentPost = posts.find(p => p.id === post.id);
+    const liked = currentPost?.liked ?? post.liked;
+
+    // TODO: handleDoubleTap 작성 (실습 3-7)
+    const handleDoubleTap = () => {
+        if (!liked) toggleLike(post.id);
+    };
 
     return (
         <ThemedView style={styles.feedMargin}>
             <FeedPostHeader user={user} />
-            <FeedImage image={resolveImageSource(post.images[0])} />
+            {/* TODO: onDoubleTap 연결 (실습 3-8) */}
+            <FeedImage
+                image={resolveImageSource(post.images[0])}
+                onDoubleTap={handleDoubleTap}
+            />
             <ContentContainer style={{ gap: 4 }}>
                 <FeedPostActions
                     postId={post.id}
